0144f53b6262e60ff7dd4a55f9fa2faeb22294bf
Artumax213 <arturo.paredes@comunidadfeliz.cl>
2026-02-10 22:27:39 -0300

    create group and group_user models and set associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :contacts, dependent: :destroy
   has_many :conversants, dependent: :destroy
-
+  # admin_groups is not a physical table but rather an association name used in the users
+  # model to facilitates the relationship between users and groups where users act as admin
+  has_many :admin_groups, class_name: "Group", foreign_key: "admin_id"
 end
---
+++ db/migrate/20260211011331_create_groups.rb
@@ -0,0 +1,12 @@
+class CreateGroups < ActiveRecord::Migration[8.0]
+  def change
+    create_table :groups do |t|
+      t.string :name
+
+      t.timestamps
+
+      # Adding the admin reference, pointing to the users table
+      t.references :admin, foreign_key: { to_table: users }
+    end
+  end
+end
