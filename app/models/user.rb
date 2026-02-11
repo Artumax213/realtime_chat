4d4ada71d729667e334147b26b3195db9dbfb01e
Artumax213 <arturo.paredes@comunidadfeliz.cl>
2026-02-10 22:41:00 -0300

    add additional associations to he users model: added_users and added_groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
   # admin_groups is not a physical table but rather an association name used in the users
   # model to facilitates the relationship between users and groups where users act as admin
   has_many :admin_groups, class_name: "Group", foreign_key: "admin_id"
+
+  # define associations for added users and groups separately
+  has_many :added_users, through: :contacts, source: :contactable, source_type: "User"
+  has_many :added_groups, through: :contacts, source: :contactable, source_type: "Group"
+
 end
