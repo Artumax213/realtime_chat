8793d3c20fa8cafa65650a9f3c0196673cf523ea
Artumax213 <arturo.paredes@comunidadfeliz.cl>
2026-02-21 20:49:24 -0300

    add name field to the users table, add Avatar and change devise signup/edit Forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_21_225017) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_21_233659) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -93,6 +93,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_21_225017) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
